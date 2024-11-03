62ecc33466ac249184188464fe75be02e7107eb1
Gnuance <179989680+Gnuance@users.noreply.github.com>
2024-11-03 12:16:01 -0800

    10 minute timestamp

--- main.cpp
+++ main.cpp
@@ -49,7 +49,7 @@ int main()
     // accumulators & counter for racing averages added for Lab26
     const int NUMBER_OF_RACES = 15;
     vector<vector<double>> vecResults;
-    double arrResults[3][4] = {0};
+    double arrResults[4][3] = {0};
 
     // off to the races. Record race results in local variables
     vector<double> readRaceResults = ReadRace(vecCodes, listCodes, setCodes, FILE_NAME);
@@ -61,10 +61,14 @@ int main()
     // create heats for each round of races
     for (int i = 0; i < NUMBER_OF_RACES; i++)
     {
-        vecResults.push_back(ReadRace(vecCodes, listCodes, setCodes, FILE_NAME));
-        vecResults.push_back(SortRace(vecCodes, listCodes, setCodes));
-        vecResults.push_back(InsertRace(vecCodes, listCodes, setCodes, INSERT_CODE));
-        vecResults.push_back(DeleteRace(vecCodes, listCodes, setCodes));
+        // get race results
+        readRaceResults = ReadRace(vecCodes, listCodes, setCodes, FILE_NAME);
+        sortRaceResults = SortRace(vecCodes, listCodes, setCodes);
+        insertRaceResults = InsertRace(vecCodes, listCodes, setCodes, INSERT_CODE);
+        deleteRaceResults = DeleteRace(vecCodes, listCodes, setCodes);
+
+        // accumulate results
+        arrResults[0][0]
     }
     
 
